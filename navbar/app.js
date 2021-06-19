762b39408dc79862ec9f8bc35652c8241dd05dbe
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-06-19 23:43:22 +0100

    Upadated

---
+++ navbar/app.js
@@ -0,0 +1,5 @@
+// gsap.to(".one", {transform: 'translateY(-100vh)', duration: .7, delay: 1.5});
+// gsap.to(".two", {transform: 'translateY(-100vh)', duration: .5, delay: 1.2});
+
+new CircleType(document.getElementById('remi'));
+new CircleType(document.getElementById('remi2'));
