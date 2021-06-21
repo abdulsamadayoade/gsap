06ad84efc918a256a11fa4000b099d4149343675
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-06-21 16:27:00 +0100

    Added fade in animation on the heading

--- navbar/app.js
+++ navbar/app.js
@@ -1,2 +1,4 @@
-gsap.to(".preloader", {transform: 'translateY(-100vh)', duration: 2, delay: 2});
-gsap.to(".header", {transform: 'translateY(-100vh)', duration: .7, delay: 2});
+CustomEase.create("hop", "M0,0 C0.83,0.134 0.92,0.24 1,1 ")
+
+gsap.to(".preloader", {transform: 'translateY(-100vh)', duration: 2, delay: 2, ease: 'hop'});
+gsap.to(".header", {transform: 'translateY(-100vh)', duration: 1.5, delay: 2, ease: 'hop'});
