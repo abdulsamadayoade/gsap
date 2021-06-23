aa9dd6df8ee6537d80171568028e825d000e48ca
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-06-23 20:21:46 +0100

    Added animations on the menu links

--- navbar/js/app.js
+++ navbar/js/app.js
@@ -3,55 +3,50 @@ CustomEase.create("hop", "M0,0 C0.83,0.134 0.92,0.24 1,1 ")
 gsap.to(".preloader", {transform: 'translateY(-100vh)', duration: 2, delay: 2, ease: 'hop'});
 gsap.to(".header", {transform: 'translateY(-100vh)', duration: 1.5, delay: 2, ease: 'hop'});
 
-
-const navLeft = document.querySelector('.nav-overlay__left');
+const home = document.getElementById('home');
 const about = document.getElementById('about');
 const product = document.getElementById('product');
 const gallery = document.getElementById('gallery');
 const contact = document.getElementById('contact');
+const img1 = document.querySelector('.img-1');
 const img2 = document.querySelector('.img-2');
 const img3 = document.querySelector('.img-3');
 const img4 = document.querySelector('.img-4');
 const img5 = document.querySelector('.img-5');
 
 
+home.addEventListener('mouseover', function () {
+    img1.classList.add('active');
+});
 
 about.addEventListener('mouseover', function () {
     img2.classList.add('active');
-    navLeft.classList.add('active');
 });
 
 about.addEventListener('mouseleave', function () {
     img2.classList.remove('active');
-    navLeft.classList.remove('active');
 });
 
 product.addEventListener('mouseover', function () {
     img3.classList.add('active');
-    navLeft.classList.add('active');
 });
 
 product.addEventListener('mouseleave', function () {
     img3.classList.remove('active');
-    navLeft.classList.remove('active');
 });
 
 gallery.addEventListener('mouseover', function () {
     img4.classList.add('active');
-    navLeft.classList.add('active');
 });
 
 gallery.addEventListener('mouseleave', function () {
     img4.classList.remove('active');
-    navLeft.classList.remove('active');
 });
 
 contact.addEventListener('mouseover', function () {
     img5.classList.add('active');
-    navLeft.classList.add('active');
 });
 
 contact.addEventListener('mouseleave', function () {
     img5.classList.remove('active');
-    navLeft.classList.remove('active');
 });
