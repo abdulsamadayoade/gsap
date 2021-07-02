ff029ff08f9c89dd961d1298b7513fb702497f61
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-07-02 23:43:20 +0100

    Updated

--- hover-effect/app.js
+++ hover-effect/app.js
@@ -17,3 +17,39 @@ $(document).ready(function () {
         });
     });
 });
+
+$('.navigation-link-1').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/1.jpeg)'});
+});
+
+$('.navigation-link-2').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/2.jpg)'});
+});
+
+$('.navigation-link-3').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/3.jpg)'});
+});
+
+$('.navigation-link-4').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/4.jpg)'});
+});
+
+$('.navigation-link-5').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/5.jpg)'});
+});
+
+$('.navigation-link-6').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/6.jpg)'});
+});
+
+$('.navigation-link-7').hover(function () {
+    $('.project-preview').css({'background-image': 'url(images/7.jpeg)'});
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop(),
+    dh = $(document).height(),
+    wh = $(window).height();
+    scrollPercent = (scroll / (dh - wh)) * 100;
+    $('.progressbar').css('height', scrollPercent + '%');
+});
