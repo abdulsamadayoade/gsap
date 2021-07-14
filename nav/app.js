780496435e40fc1412e99935d8c95bb1460ebdac
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-07-14 23:22:03 +0100

    Updated to GSAP 3

--- nav/app.js
+++ nav/app.js
@@ -1,36 +1,46 @@
-let tl = new TimelineMax({paused: true});
+let tl = gsap.timeline({ paused: true });
 
-tl.to('.one', 0.8, {
+tl.to('.one', {
+    duration: 0.8,
     y: 6,
     rotation: 45,
     ease: Expo.easeInOut
 });
 
-tl.to('.two', 0.8, {
+tl.to('.two', {
+    duration: 0.8,
     y: -6,
     rotation: -45,
     ease: Expo.easeInOut,
     delay: -0.8
 });
 
-tl.to('.menu', 2, {
+tl.to('.menu', {
+    duration: 2, 
     top: '0%',
-    ease: Expo.easeOut,
+    ease: Expo.easeInOut,
     delay: -2
 });
 
-tl.staggerFrom('.menu ul li', 2, {
+
+tl.from('.menu ul li', {
+    duration: 2,
     x: -200,
     opacity: 0,
-    ease: Expo.easeInOut
-}, 0.3);
+    ease: Expo.easeInOut,
+    stagger: 0.3
+});
 
 tl.reverse();
 
-$(document).on('click', '.toggle-btn', function () {
+const toggleBtn = document.querySelector('.toggle-btn');
+toggleBtn.addEventListener('click', function () {
     tl.reversed(!tl.reversed());
 });
 
-$(document).on('click', 'a', function () {
-    tl.reversed(!tl.reversed());
+const links = document.querySelectorAll('a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        tl.reversed(!tl.reversed());
+    });
 });
