9ea8d50e6c1b851a849a5a8e32b45306bf449e20
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-08-24 15:21:53 +0100

    Updated

--- menu-hover-effect-2/app.js
+++ menu-hover-effect-2/app.js
@@ -1,28 +1,68 @@
-const tl = gsap.timeline();
-tl.from('.line p', 2.8,{
-    delay: 1,
-    ease: 'power4.out',
-    y: 250,
-    skewY: 15,
-    stagger: {
-        amount: .8
-    }
+gsap.to('.header', {
+    backgroundColor: '#c9c9c9',
+    duration: 2,
+    delay: .5,
 });
 
-tl.to('.body',1.8,{
-    backgroundColor: '#c9c9c9'
-});
-
-tl.to('.line p a', 1.5,{
-    color: 'black'
-},"-=1.8");
-
-tl.from('.logo, .ul li',1.2,{
+gsap.from('.header__nav', {
+    y: '-100%',
     opacity: 0,
-    y: "-100%",
+    duration: 1.5,
     stagger: {
         amount: .4
     }
+});
+
+const tl = gsap.timeline({
+    paused: true
+});
+
+tl.to('.overlay-1', {
+    height: '100%'
+});
+
+tl.to('.overlay-2', {
+    height: '100%'
+});
+
+tl.to('.overlay-3', {
+    height: '100%'
+});
+
+tl.to('.overlay-4', {
+    height: '100%'
+});
+
+const tl1 = gsap.timeline({
+    paused: true
+});
+
+tl1.to('.header__logo, .btn', {
+    color: '#fff'
+});
+
+tl1.to('.menu', {
+    y: '0%',
+    delay: 2
+});
+
+const openBtn = document.querySelector('.btn--open');
+const closeBtn = document.querySelector('.btn--close');
+
+openBtn.addEventListener('click', () => {
+    openBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+
+    tl.play();
+    tl1.play();
+});
+
+closeBtn.addEventListener('click', () => {
+    openBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
+
+    tl.reverse();
+    tl1.reverse();
 });
 
 follower = document.querySelectorAll('.cursor');
@@ -40,8 +80,8 @@ TweenMax.to({},0.016,{
 
         TweenMax.set(follower, {
             css: {
-                left: posX - 80,
-                top: posY - 80
+                left: posX - 400,
+                top: posY - 200
             }
         });
     }
